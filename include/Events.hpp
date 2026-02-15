b871ffe4b72433cc5e3fe3b3329a25f9874ea4ee
Fire <17692105+FireMario211@users.noreply.github.com>
2026-02-14 20:17:35 -0500

    Change OnSogLayer to use Event instead of SimpleEvent (#10)

--- include/Events.hpp
+++ include/Events.hpp
@@ -2,7 +2,7 @@
 #include "layers/SogLayer.hpp"
 
 namespace soggy_mod {
-    struct OnSogLayer final : geode::SimpleEvent<OnSogLayer, SogLayer*> {
-        using SimpleEvent::SimpleEvent;
+    struct OnSogLayer final : geode::Event<OnSogLayer, bool(SogLayer*)> {
+        using Event::Event;
     };
 }
